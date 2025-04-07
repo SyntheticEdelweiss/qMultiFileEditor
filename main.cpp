c94bc28297cd5b843164885259ae32d66ba1b3f4
SyntheticEdelweiss <SyntheticEdelweiss@proton.me>
2025-04-07 08:34:40 +0300

    Set current dir to .exe dir

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
 {
     QApplication::setStyle("Fusion");
     QApplication app(argc, argv);
+    QDir::setCurrent(qApp->applicationDirPath());
 
     QTimer::singleShot(0, &app, [](){
         MultiFileEditor* w = new MultiFileEditor;
